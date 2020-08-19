5b3b07df3e3daf95b5249af88909b9c5a2f9cd1d
jvallee <jasonmvallee@gmail.com>
2020-08-19 14:24:05 -0500

    Updates

---
+++ precache-manifest.a13556a39917e5db189d535daaa83165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2216fc15ca2ea2a2806f00d6c425dabc",
+    "url": "/calculatorreact/index.html"
+  },
+  {
+    "revision": "d2a22db4f15dfbd1ea2d",
+    "url": "/calculatorreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c330915d15098f908d2c",
+    "url": "/calculatorreact/static/js/2.30292aa5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calculatorreact/static/js/2.30292aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a22db4f15dfbd1ea2d",
+    "url": "/calculatorreact/static/js/main.6519fcfd.chunk.js"
+  },
+  {
+    "revision": "42e6ad35d5e293346945",
+    "url": "/calculatorreact/static/js/runtime-main.ecc283a5.js"
+  }
+]);

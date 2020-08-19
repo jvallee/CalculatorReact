2247d70f4e935876cad49749ef0e52154ce89453
jvallee <jasonmvallee@gmail.com>
2020-08-19 14:00:23 -0500

    Updates

---
+++ precache-manifest.b8d2eef66c308b3e37b6e8ef8ea0350d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e97d87247a8474e1764047dea720f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c30dd225f2f3fe4e4289",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c330915d15098f908d2c",
+    "url": "/static/js/2.30292aa5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.30292aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30dd225f2f3fe4e4289",
+    "url": "/static/js/main.a17b42a2.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
